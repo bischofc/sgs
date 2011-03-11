593aee93a18e0c702a638e3d40e0a7917bc35ede
Christian Bischof <bischof.christian@gmail.com>
2011-03-11 13:09:20 +0100

    added copyright notice

--- src/simulation/producers/ProducerOwner.h
+++ src/simulation/producers/ProducerOwner.h
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2010 Christian Bischof (bischof.christian@gmail.com)
+This file is part of "Smart Grid Simulator".
+
+"Smart Grid Simulator" is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+"Smart Grid Simulator" is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with "Smart Grid Simulator".  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef simulation_endpoint_producer_producerOwner_h
 #define simulation_endpoint_producer_producerOwner_h
 
